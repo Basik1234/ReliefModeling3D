d853f3c850abce408e3aae765fe73a73cf2dc1a7
Basik <Basik-cs@mail.ru>
2018-01-26 20:26:26 +1000

    add VIew3D

---
+++ ReliefModeling/ReliefModeling/Model/Controls/View3D.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenTK;
+
+namespace ReliefModeling.Model.Controls
+{
+    public class View3D : GLControl
+    {
+        private Shape _shape;
+
+        public View3D(Shape shape)
+        {
+            _shape = shape;
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+        }
+    }
+}
