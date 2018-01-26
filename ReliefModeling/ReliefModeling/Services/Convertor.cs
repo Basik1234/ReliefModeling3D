82576f271a9e94a8f94623db922b9249dc60ead9
Basik <Basik-cs@mail.ru>
2018-01-26 15:22:18 +1000

    Add services

---
+++ ReliefModeling/ReliefModeling/Services/Convertor.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Media.Imaging;
+using ReliefModeling.Model;
+
+namespace ReliefModeling.Services
+{
+    public static class Convertor
+    {
+        public static BitmapImage3D Convert2DTo3D(BitmapImage bitmap2D)
+        {
+            BitmapImage3D bitmap3D = new BitmapImage3D();
+
+            return bitmap3D;
+        }
+    }
+}
